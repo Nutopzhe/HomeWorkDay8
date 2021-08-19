1a85665188ba6f99827e9ff118ab272cb5679e81
Nikolay Rybakov <mr.Nikolay.Rybakov@yandex.ru>
2021-08-20 00:47:36 +0300

    Added sorting by the length of the sum of lines. Added comments

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,19 @@ public class Main {
         baggageList.add(new Baggage("Red Wings", "Эконом", 7));
         baggageList.add(new Baggage("S7", "Первый", 20));
 
+        //сортировка с помощью анонимного класса по весу багажа
         baggageList.sort(new Comparator<Baggage>() {
             @Override
             public int compare(Baggage o1, Baggage o2) {
                 return o1.getWeight() - o2.getWeight();
             }
         });
+
+        System.out.println(baggageList);
+
+        //сортировка с помощью лямбды по сумме длин строк перевозчика и класса перелета
+        baggageList.sort((o1, o2) -> (o2.getCarrier().length() + o2.getFlight().length()) -
+                (o1.getCarrier().length() + o1.getFlight().length()));
 
         System.out.println(baggageList);
     }
